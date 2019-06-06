8b55d439807ea3eb2616a0ad42b1f2b2295c31af
naiveallen <pyyuanye@gmail.com>
2019-06-05 21:40:54 -0700

    add AJAX to cart

--- app/controllers/lineitems_controller.rb
+++ app/controllers/lineitems_controller.rb
@@ -32,7 +32,12 @@ class LineitemsController < ApplicationController
     @line_item = @cart.add_item(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to shopper_index_path, notice: 'Item was successfully added to cart.' }
+        format.js
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
 
