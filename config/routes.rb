dd1b05d4604b96d27c3f7344e4957cafde420b19
naiveallen <pyyuanye@gmail.com>
2019-06-06 17:15:45 -0700

    authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  get 'login', to: 'access#new'
+  post 'login', to: 'access#create'
+  get 'logout', to: 'access#destroy'
+  post 'logout', to: 'access#destroy'
+  get 'admin', to: 'admin#index'
+
   resources :users
   resources :orders
   resources :lineitems
   resources :carts
-  get 'shopper/index'
+  get 'shopper', to: 'shopper#index'
   resources :products
+  root "shopper#index", as: 'root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
